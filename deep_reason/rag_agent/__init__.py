295f2a8e468955f312678e1d5746cbf633979e1a
ngc436 <mariyaxod@yandex.ru>
2025-03-28 22:07:47 +0300

    remove excessive info

--- deep_reason/rag_agent/__init__.py
+++
@@ -1,4 +0,0 @@
-from deep_reason.rag_agent.agent import RAGAgent
-from deep_reason.rag_agent.chains import ElasticsearchDocumentEncoder, build_retrieval_chain, RAGDocumentResponse
-
-__all__ = ['RAGAgent', 'ElasticsearchDocumentEncoder', 'build_retrieval_chain', 'RAGDocumentResponse'] 
